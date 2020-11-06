1aeb61d7fdf57dd48cf4c9ebe9f636acd8e1d673
maria <magsend@gmail.com>
2020-11-06 23:46:37 +0300

    create/update terms frequency data sample from twitter github

--- db/update_db/update.py
+++ db/update_db/update.py
@@ -1,32 +1,69 @@
 from datetime import date, timedelta
-import glob
 import pandas as pd
+import argparse
+import datetime
 
-sdate = date(2008, 8, 15)   # start date
-edate = date(2008, 9, 15)   # end date
+url = "https://raw.githubusercontent.com/thepanacealab/covid19_twitter/master/dailies/"
+attrs = [
+         {'type': '_top1000terms',
+          'name': 'all_terms.csv'},
+         {'type': '_top1000bigrams',
+          'name': 'all_bigrams.csv'},
+         {'type': '_top1000trigrams',
+          'name': 'all_trigrams.csv'}
+         ]
 
-delta = edate - sdate       # as timedelta
 
-for i in range(delta.days + 1):
-    day = sdate + timedelta(days=i)
-    print(day)
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--from', dest='from_date', type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'),
+        default=datetime.date.today() - datetime.timedelta(days=1))
+    parser.add_argument(
+        '--to', dest='to_date', type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'),
+        default=datetime.date.today())
+    parser.add_argument(
+        '--mode', dest='mode', type=str, default='update')
+
+    args = parser.parse_args()
+    from_date = args.from_date
+    to_date = args.to_date
+
+    delta = to_date - from_date       # as timedelta
+
+    for item in attrs:
+
+        if args.mode == 'update':
+            df = pd.read_csv('../' + item['name'], index_col=[0])
+            result = pd.concat([df, collect_data(item['type'], delta, from_date)])
+            result.to_csv('../' + item['name'])
+        elif args.mode == 'new':
+            df = collect_data(item['type'], delta, from_date)
+            df.to_csv('../' + item['name'])
 
 
-#download link
-#https://github.com/thepanacealab/covid19_twitter/tree/master/dailies/2020-10-01
-#https://github.com/thepanacealab/covid19_twitter/blob/master/dailies/2020-10-01/2020-10-01_top1000bigrams.csv
-#https://github.com/thepanacealab/covid19_twitter/blob/master/dailies/2020-10-01/2020-10-01_top1000terms.csv
-#https://github.com/thepanacealab/covid19_twitter/blob/master/dailies/2020-10-01/2020-10-01_top1000trigrams.csv
 
-path = '/Users/maria/PyCharmProjects/covid19_twitter/dailies'
-tmp = []
-for d in glob.glob(path + "/*/"):
-    for f in glob.glob(d + '*_top1000terms.csv'):
-        tmp_df = pd.read_csv(f, index_col=None, header=0, names=['term', 'counts'])
-        f = f.replace(d, '')
-        tmp_df['date'] = f[0:10]
+
+def collect_data(name, delta, from_date):
+
+    tmp = []
+
+    for i in range(delta.days + 1):
+        day = from_date + timedelta(days=i)
+        path = url + str(day).split(' ')[0] + '/' + str(day).split(' ')[0] + name + '.csv'
+        tmp_df = pd.read_csv(path, error_bad_lines=False, index_col=None, header=0, names=['term','counts'])
+        tmp_df['date'] = str(day).split(' ')[0]
         tmp.append(tmp_df)
 
-df = pd.concat(tmp, axis=0, ignore_index=True)
-df['counts'] = pd.to_numeric(df['counts'], errors='coerce')
-df['date'] = pd.to_datetime(df['date'], errors='coerce')
+    df = pd.concat(tmp, axis=0, ignore_index=True)
+    df['counts'] = pd.to_numeric(df['counts'], errors='coerce')
+    df['date'] = pd.to_datetime(df['date'], errors='coerce')
+    df.reset_index(inplace=True, drop=True)
+
+    return df
+
+
+
+if __name__ == "__main__":
+    main()
